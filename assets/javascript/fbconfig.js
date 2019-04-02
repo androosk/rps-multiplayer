5b3236cd97fa2544ce3e088db5d87d0b6f23cfc6
androosk <46608870+androosk@users.noreply.github.com>
2019-04-02 18:19:50 -0400

    basic functionality complete

---
+++ assets/javascript/fbconfig.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCKWJJGheGiFj5NOhCIsM1DMyJZXTp2P7E",
+    authDomain: "rps-multiplayer-b330a.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-b330a.firebaseio.com",
+    projectId: "rps-multiplayer-b330a",
+    storageBucket: "rps-multiplayer-b330a.appspot.com",
+    messagingSenderId: "907022823562"
+  };
+  firebase.initializeApp(config);
